d77d8953c3673270f9aefe86abab5f1e2ec388d7
LENOVO_Bankify <pragyan9423@gmail.com>
2023-11-10 22:48:26 +0530

    initial commit

--- app/src/main/java/com/ocean/mybottomnavbar/adapter/RowOneRVadapter.kt
+++ app/src/main/java/com/ocean/mybottomnavbar/adapter/RowOneRVadapter.kt
@@ -1,4 +1,24 @@
 package com.ocean.mybottomnavbar.adapter
 
-class RowOneRVadapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ocean.mybottomnavbar.databinding.AdapterHomeRowOneBinding
+import com.ocean.mybottomnavbar.databinding.AdapterHomeRowTwoBinding
+
+class RowOneRVadapter: RecyclerView.Adapter<RowOneRVadapter.ViewHolder>() {
+    class ViewHolder(private val binding: AdapterHomeRowOneBinding): RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RowOneRVadapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RowOneRVadapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
